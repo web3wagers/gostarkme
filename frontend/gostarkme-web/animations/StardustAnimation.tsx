ae6e098b97815705fb4ef53cba884437b66e95fc
adrianvrj <adrivargas48@gmail.com>
2024-09-02 13:49:53 -0600

    [fix] canvas might be null

--- frontend/gostarkme-web/animations/StardustAnimation.tsx
+++ frontend/gostarkme-web/animations/StardustAnimation.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 
 export const StardustAnimation = () => {
-    const canvasRef = useRef(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
   
     useEffect(() => {
       const canvas = canvasRef.current;
