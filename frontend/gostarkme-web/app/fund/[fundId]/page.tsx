732f18b374649759baeb043300861c8d8084be68
JoelVR17 <vermudo.com@gmail.com>
2024-09-26 12:41:46 -0600

    feat: create the fund details page

--- frontend/gostarkme-web/app/fund/[fundId]/page.tsx
+++ frontend/gostarkme-web/app/fund/[fundId]/page.tsx
@@ -1,7 +1,15 @@
+import Fund from "@/components/modules/Fund/Fund";
+import Bounded from "@/components/ui/Bounded";
+import Divider from "@/components/ui/Divider";
+
 const FundDetailsPage = async ({ params }: { params: { fundId: string } }) => {
   return (
     <>
-      <h1 className="text-4xl mb-3 font-black">{params.fundId}</h1>
+      <Bounded className="px-60 text-lg">
+        <h2 className="font-bold">User's fund - {params.fundId}</h2>
+        <Divider />
+        <Fund message="I need some Starks to finish my project."></Fund>
+      </Bounded>
     </>
   );
 };
