1e0d06c231f0e9318174ce279fe2f59eafb2a145
Mariangela N <mariangelanavarro10@gmail.com>
2024-09-27 21:12:12 -0600

    Change message placeholder

--- frontend/gostarkme-web/components/modules/newfunding/DescriptionStep.tsx
+++ frontend/gostarkme-web/components/modules/newfunding/DescriptionStep.tsx
@@ -12,7 +12,7 @@ const DescriptionStep: React.FC<DescriptionStepProps> = ({
   <div className="w-11/12 md:w-3/3 lg:w-1/1 font-sans text-2xl font-normal leading-normal text-left">
     <strong>Note:</strong> You can always edit your funding description later ;)
     <textarea
-      placeholder="Set your goal in STRKs"
+      placeholder="Funding description"
       value={fundingDescription}
       onChange={(e) => setFundingDescription(e.target.value)}
       className="mt-3 p-4 border border-black rounded w-full h-60 resize-none font-sans font-light leading-snug text-left placeholder:text-base placeholder:font-light"
