8bb0321645879cc827e0618a46aed5d5c6c04a0d
josue.271168@gmail.com <jsandinoDevjsandinoDevjsandinoDev>
2024-09-28 18:46:41 -0600

    [docs] Add Frontend Setup Documentation

--- frontend/gostarkme-web/app/fund/[fundId]/page.tsx
+++ frontend/gostarkme-web/app/fund/[fundId]/page.tsx
@@ -6,7 +6,7 @@ const FundDetailsPage = async ({ params }: { params: { fundId: string } }) => {
   return (
     <>
       <Bounded className="px-60 text-lg">
-        <h2 className="font-bold">User's fund - {params.fundId}</h2>
+        <h2 className="font-bold">User&apos;s fund - {params.fundId}</h2>
         <Divider />
         <Fund message="I need some Starks to finish my project."></Fund>
       </Bounded>
