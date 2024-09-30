9f45dd23a437eeb191ce3eda8d0cfd629b4ed04c
William Ugalde Gamboa <wugalde19@gmail.com>
2024-09-29 22:40:59 -0600

    feat: Display user funds in My Funds page

--- frontend/gostarkme-web/app/app/myfunds/[useraddr]/page.tsx
+++ frontend/gostarkme-web/app/app/myfunds/[useraddr]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FundCard from '@/components/modules/myfunds/FundCard';
 import Navbar from '@/components/ui/Navbar';
 import Footer from '@/components/ui/Footer';
 import { Button } from '@/components/ui/Button';
@@ -10,7 +11,35 @@ interface MyFundsPageProps {
 }
 
 const MyFundsPage: React.FC<MyFundsPageProps> = ({ params }) => {
+  const [funds, setFunds] = useState<object[]>([]);
   const { useraddr } = params;
+
+  useEffect(() => {
+    // TODO: Implement funds fetching using 'useraddr'
+    setFunds([
+      {
+        id: 1,
+        type: "Project",
+        title: "Batman's fund",
+        description: "Example of card without delete button",
+        onClick: handleDeleteFund
+      },
+      {
+        id: 2,
+        type: "Project",
+        title: "Deadpool's fund",
+        description: "I need help with my project to develop an awesome project like Go Stark Me",
+        onClick: handleDeleteFund
+      },
+      {
+        id: 3,
+        type: "Fund",
+        title: "Spider-Man's fund",
+        description: "Text to prove that we add elipsis when text exceds card width and we don't break the layout. asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf",
+        onClick: handleDeleteFund
+      }
+    ]);
+  }, []);
 
   const navItems = [
     { label: 'My Profile', href: `/app/myprofile/${useraddr}` },
@@ -20,6 +49,11 @@ const MyFundsPage: React.FC<MyFundsPageProps> = ({ params }) => {
   const onNewFundHandler = () => {
     // TODO: Implement new fund action
     alert(`Creating new fund`);
+  }
+
+  const handleDeleteFund = (fundId: number) => {
+    // TODO: Implement fund deletion based on a unique id or receive the fund object and delete it
+    alert(`Deleting fund with id: ${fundId}`);
   }
 
   return (
@@ -40,13 +74,27 @@ const MyFundsPage: React.FC<MyFundsPageProps> = ({ params }) => {
             <div className="flex items-center">
               <h1 className="text-2xl font-bold mr-2">My Funds &#10024;</h1>
             </div>
-            <Button label='New' onClick={onNewFundHandler} />
+            <Button className='!px-12 !py-2.5' label='New' onClick={onNewFundHandler} />
           </div>
-          <div className="flex justify-center items-center h-64">
-            <div className="text-center text-gray-500">
-              No funds found for address {useraddr.slice(0, 5)}...{useraddr.slice(-4)}
+          {funds.length === 0 ? (
+            <div className="flex justify-center items-center h-64">
+              <div className="text-center text-gray-500">
+                No funds found for address {useraddr.slice(0, 5)}...{useraddr.slice(-4)}
+              </div>
             </div>
+          ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 gap-x-16">
+            {funds.map((fund: any, index: number) => (
+              <FundCard 
+                key={index} 
+                type={fund.type} 
+                title={fund.title} 
+                description={fund.description} 
+                onClick={fund.onClick}
+              />
+            ))}
           </div>
+        )}
         </div>
       </main>
       <Footer />
