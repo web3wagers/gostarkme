bf3489da408144a3aec47bb0c716c12270c26013
Luis Sanchez <luissanchezzamora23@gmail.com>
2024-09-30 08:58:01 -0500

    [fix] change emoji to unicode

--- frontend/gostarkme-web/app/dashboard/page.tsx
+++ frontend/gostarkme-web/app/dashboard/page.tsx
@@ -39,7 +39,7 @@ const Dashboard = () => {
     <div className="container mx-auto p-6 min-h-screen">
       <h1 className="text-4xl font-bold mb-6 flex items-center">
         Latest Funds
-        <span className="ml-2 text-yellow-400">✨</span>
+        <span className="ml-2 text-yellow-400">&#x2728;</span>
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6 md:gap-x-[138px] md:gap-y-[84px]">
         {funds.map((fund, index) => (
