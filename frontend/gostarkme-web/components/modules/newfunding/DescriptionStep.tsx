888d14e3f9f754f4afa708188813f1072701e670
Josué Brenes <josuebrenes3005@gmail.com>
2024-09-30 23:17:32 -0600

    fix: add back button in new fund fase 2 page

--- frontend/gostarkme-web/components/modules/newfunding/DescriptionStep.tsx
+++ frontend/gostarkme-web/components/modules/newfunding/DescriptionStep.tsx
@@ -9,8 +9,9 @@ const DescriptionStep: React.FC<DescriptionStepProps> = ({
   fundingDescription,
   setFundingDescription,
 }) => (
-  <div className="w-11/12 md:w-3/3 lg:w-1/1 font-sans text-2xl font-normal leading-normal text-left">
+  <div className="relative w-11/12 md:w-3/3 lg:w-1/1 font-sans text-2xl font-normal leading-normal text-left">
     <strong>Note:</strong> You can always edit your funding description later ;)
+
     <textarea
       placeholder="Funding description"
       value={fundingDescription}
@@ -18,6 +19,18 @@ const DescriptionStep: React.FC<DescriptionStepProps> = ({
       className="mt-3 p-4 border border-black rounded w-full h-60 resize-none font-sans font-light leading-snug text-left placeholder:text-base placeholder:font-light"
       required
     />
+
+    {/* Back Arrow */}
+    <a
+      href=""
+      className="absolute right-[-270px] top-[-205px]" 
+    >
+      <img 
+        src="/icons/arrow-right.png" 
+        alt="Back Arrow"
+        className="h-8 w-8"
+      />
+    </a>
   </div>
 );
 
