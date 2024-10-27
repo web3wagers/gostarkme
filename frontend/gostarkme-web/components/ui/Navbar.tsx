9557f9d147f55dd0591c13a7b2de563f0f7cd9a7
Darshan Chauhan <46072647+GradleD@users.noreply.github.com>
2024-10-27 17:27:55 +0530

    Update Navbar.tsx

--- frontend/gostarkme-web/components/ui/Navbar.tsx
+++ frontend/gostarkme-web/components/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
 import ConnectWallet from "../ui/ConnectWalletButton"
+import { navItems } from '../constants/navItems'; 
 
 interface NavItem {
   label: string;
