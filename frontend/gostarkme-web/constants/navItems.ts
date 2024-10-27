12e0621e6414acfacc56ed2806b1fe9e69138074
Darshan Chauhan <46072647+GradleD@users.noreply.github.com>
2024-10-27 17:30:21 +0530

    Create navItems.ts

---
+++ frontend/gostarkme-web/constants/navItems.ts
@@ -0,0 +1,15 @@
+export const navItems = [
+  {
+    label: 'Home',
+    href: '/home',
+  },
+  {
+    label: 'About',
+    href: '/about',
+  },
+  {
+    label: 'Contact',
+    href: '/contact',
+  },
+  // Add more nav items as needed
+];
