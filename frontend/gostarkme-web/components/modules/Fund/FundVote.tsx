37c54bb883e8e3cfa748ffbdb6b0a46c5b9f9282
Manuel JG <mannyb1999@hotmail.com>
2024-11-04 20:09:16 -0600

    Worked on deleting unused imports

--- frontend/gostarkme-web/components/modules/Fund/FundVote.tsx
+++ frontend/gostarkme-web/components/modules/Fund/FundVote.tsx
@@ -1,12 +1,10 @@
 import { calculatePorcentage } from "@/app/utils";
 import { Button } from "@/components/ui/Button";
-import { LinkButton } from "@/components/ui/LinkButton";
 import ProgressBar from "@/components/ui/ProgressBar";
 import { fundAbi } from "@/contracts/abis/fund";
 import { walletStarknetkitLatestAtom } from "@/state/connectedWallet";
 import { useAtomValue } from "jotai";
-import { useState } from "react";
-import { Contract, wallet, InvokeFunctionResponse } from "starknet";
+import { Contract, InvokeFunctionResponse } from "starknet";
 
 interface FundVoteProps {
   upVotes: number,
